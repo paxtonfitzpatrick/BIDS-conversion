0d28b19fc5ece579ab8a7c8f20a12133f2280fe6
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2018-12-14 15:52:35 -0500

    moved functions to separate file

---
+++ bidsify/bidsify.py
@@ -0,0 +1,6 @@
+import sys
+import argparse
+
+# argparse setup for calling from command line
+parser = argparse.ArgumentParser(description='Convert data to BIDS format')
+parser.add_argument('origpath', type=str, )
